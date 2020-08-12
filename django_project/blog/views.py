c50ff60ee57c710b4b92b0d381f448662df96ae4
Jake Anderson <jake94a@gmail.com>
2020-08-11 22:27:27 -0600

    User Registration

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jake',
-        'title': 'My Declaration',
-        'content': 'Madi got cake',
-        'date_posted': 'July 31, 2020'
-    }, {
-        'author': 'Madi95a',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 32, 2020'
-    }
-]
-
-
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
